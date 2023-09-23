e4b1b42e8866686167d9b92cba093a0a514fe688
Manuel Duarte <duarte.manuel10@gmail.com>
2023-09-23 12:47:04 -0300

    se agrega captura de proyetos

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,4 +1,6 @@
+import kSurf from "../img/k-surf.png";
 import styles from "./Proyects.module.css";
+
 function Proyects() {
   return (
     <div className="container mt-5">
@@ -6,7 +8,21 @@ function Proyects() {
         <h2 className={styles.section_title}>GALLERY</h2>
         <p className={styles.main_title}>PROYECTS</p>
       </div>
-      <div className={styles.skill_box}></div>
+      <div className={styles.skill_box}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <img className={styles.image} src={kSurf} alt="" />
+            </div>
+            <div className="col-lg-4">
+              <img className={styles.image} src={kSurf} alt="" />
+            </div>
+            <div className="col-lg-4">
+              <img className={styles.image} src={kSurf} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
