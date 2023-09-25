bcef4b2c926303386a9a6d692ef9ba39588224ca
Manuel Duarte <duarte.manuel10@gmail.com>
2023-09-25 19:10:14 -0300

    se añade seccion contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,20 @@ function Contact() {
         <h2 className={styles.section_title}>CONTACT</h2>
         <p className={styles.main_title}>CONTACT ME</p>
       </div>
-      <div className={styles.contact_box}></div>
+      <div className={styles.contact_box}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <h2>Location:</h2>
+              <h2>Email:</h2>
+              <h2>Linkedin:</h2>
+              <h2>Whatsapp:</h2>
+              <h2>GitHub:</h2>
+            </div>
+            <div className="col-lg-6"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
