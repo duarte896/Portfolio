dce3d0d81ad8534560e807dfe507886cc0514cab
Manuel Duarte <duarte.manuel10@gmail.com>
2023-11-15 19:26:16 -0300

    contenido de about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,10 @@ function About() {
       <div>
         <h2 className={styles.section_title}>ABOUT</h2>
         <p className={styles.main_title}>ABOUT ME</p>
+        <p>
+          Im 21 Full Stack Software Developer hows extremly motivated to keep
+          learning{" "}
+        </p>
       </div>
     </div>
   );
